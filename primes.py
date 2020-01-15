63b8b011605e00727748fa9ceab1e207698657ab
Awerito <diegoignacio.munoz@alumnos.ulagos.cl>
2020-01-14 21:05:04 -0300

    solve bug in primes script

--- primes.py
+++ primes.py
@@ -1,17 +1,13 @@
-import time as t
 import math as m
 
 def prime(n):
 	""" Return True if a number n is prime, "False" otherwise. """
-	if n<=1:
-		return False
 	if n==2:
 		return True
-	if n%2==0:
+	if n<=1 or n%2==0:
 		return False
-	max_div = m.floor(m.sqrt(n))
-	itera = m.floor(max_div / 2 + 1)
-	for i in range(3, itera,  2):
-		if n % i == 0  or n % (itera + 1) == 0:
+	maxdiv = m.floor(m.sqrt(n)) + 1
+	for i in range(3, maxdiv,  2):
+		if n % i == 0:
 			return False
 	return True
