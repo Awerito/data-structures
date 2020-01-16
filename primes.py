0598c43f96ad64f79fa5d5d118b309af0af66ca3
Awerito <diegoignacio.munoz@alumnos.ulagos.cl>
2020-01-16 17:08:13 -0300

    Remove math dependency

--- primes.py
+++ primes.py
@@ -1,12 +1,10 @@
-import math as m
-
 def prime(n):
 	""" Return True if a number n is prime, "False" otherwise. """
 	if n==2:
 		return True
 	if n<=1 or n%2==0:
 		return False
-	maxdiv = m.floor(m.sqrt(n)) + 1
+	maxdiv = int(n ** (0.5)) + 1
 	for i in range(3, maxdiv,  2):
 		if n % i == 0:
 			return False
