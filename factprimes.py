e50aeff98b0132eddbfcd4d8c236a9453e507eea
Awerito <diegoignacio.munoz@alumnos.ulagos.cl>
2020-01-18 19:41:05 -0300

    Prime factorization script done

--- factprimes.py
+++ factprimes.py
@@ -1,26 +1,21 @@
 from primes import prime as pr
 
-cache = {0:3}
-def fact(n, a = {}):
+cache = {0:2, 1:3}
+def fact(n, a = []):
 	if n == 1:
 		return a
-	if n in cache:
-		a[len(a)] = n
-		return fact(1, a)
-	if n % 2 == 0:
-		a[len(a)] = 2
-		return fact(n / 2, a)
+	for i in cache:
+		if n == cache[i]:
+			a.append(n)
+			return fact(1, a)
+	print("buscando primos")
 	aux = cache[len(cache) - 1]
 	if n > aux:
 		for i in range(aux + 2, n + 1, 2):
 			if pr(i):
 				cache[len(cache)] = i
+	print("fin busqueda primos")
 	for p in cache:
-		if n % p == 0:
-			a[len(a)] = p
-			return fact(n / p, a)
-
-# Test
-print(fact(1))
-print(fact(2))
-print(fact(8))
+		if n % cache[p] == 0:
+			a.append(cache[p])
+			return fact(int(n / cache[p]), a)
