9143a41570d77b87cc9c3d625b944b05fa6e3b19
Awerito <diegoignacio.munoz@alumnos.ulagos.cl>
2020-01-27 15:13:18 -0300

    Cleaning

--- prefix-code.py
+++ prefix-code.py
@@ -4,15 +4,15 @@ import random as rnd
 s = [['a'], ['b'], ['c'], ['d'], ['e'], ['f'], ['g'], ['h'], ['i'], ['j'], ['k'], ['l'], ['m'], ['n'], ['o'], ['p'], ['q'], ['r'], ['s'], ['t'], ['u'], ['v'], ['w'], ['x'], ['y'], ['z']]
 
 for i in range(len(s)):
-	s[i].append(rnd.randint(1,len(s)))
+    s[i].append(rnd.randint(1,len(s)))
 
 size = lambda leng: leng[1]
 s.sort(key=size)
 
 s[0].append(0)
 for i in range(1, len(s)):
-	code = (s[i-1][2] + 1) * (2**(s[i][1] - s[i-1][1]))
-	s[i].append(code)
+    code = (s[i-1][2] + 1) * (2**(s[i][1] - s[i-1][1]))
+    s[i].append(code)
 
 for l in s:
-	print(l)
+    print(l)
--- test.py
+++
@@ -1,5 +0,0 @@
-def function(n):
-	return n
-
-if __name__=="__main__":
-	print(function(77))
