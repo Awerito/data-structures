36bd678279cc391605acfdc79bf9b4705d8e1614
Awerito <diegoignacio.munoz@alumnos.ulagos.cl>
2020-01-27 19:04:26 -0300

    More formality

--- sequences/remy.py
+++ sequences/remy.py
@@ -1,6 +1,11 @@
 def a(n):
-	r = 0
-	for i in range(1, n):
-		if n & i:
-			r += 1
-	return r
+    r = 0
+    for i in range(1, n):
+        if n & i:
+            r += 1
+    return r
+
+if __name__=="__main__":
+    total = 100
+    for i in range(1, total + 1):
+        print(a(i))
