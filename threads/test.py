5be328c663f281a429a4c56c0841add83af5db6e
Awerito <diegoignacio.munoz@alumnos.ulagos.cl>
2020-01-28 19:33:42 -0300

    Playing with threads

---
+++ threads/test.py
@@ -0,0 +1,25 @@
+from primes import prime as p
+import concurrent.futures as cf
+from time import time as t
+
+def test(n):
+    print(n, ":", p(n))
+
+if __name__ == "__main__":
+    threads = 1
+    total = 1000000
+    print("=============================")
+    t0 = t()
+    for i in range(1, total + 1):
+        print(i, ":", p(i))
+    t1 = t()
+    print("Single-thread", t1 - t0)
+    print("=============================")
+    """
+    t0 = t()
+    with cf.ThreadPoolExecutor(max_workers=threads) as executor:
+        executor.map(test, range(1, total + 1))
+    t1 = t()
+    print("Multi-thread", t1 - t0)
+    print("=============================")
+    """
